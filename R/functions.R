c1aebe84a6cd762391aebbd28e08b8cb16e0aedc
JDChallenger <joseph.d.challenger@gmail.com>
2025-01-23 15:52:06 +0000

    create intial thing

---
+++ R/functions.R
@@ -0,0 +1,15 @@
+#
+#' Square a chuffing number
+#'
+#' What she said
+#'
+#' @param x number to be squared
+#'
+#' @return A squared number
+#' @export
+#'
+#' @examples
+#' square()
+square <- function(x){
+  x*x
+}
---
+++ tests/testthat/test-functions.R
@@ -0,0 +1,5 @@
+test_that("multiplication works", {
+  expect_equal(square(10), 100)
+  expect_equal(square(-10), 100)
+  expect_equal(0, 0)
+})
